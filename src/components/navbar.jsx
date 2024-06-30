ebf54b3c1701cc2252e1f6a6f23a10fab67a09bc
Matias Aguirre <matias.aguirre269@gmail.com>
2024-06-30 18:49:43 -0300

    agregando navbar  como componente

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <>
+        <ul>
+                <li>home</li>
+                <li>about</li>
+                <li>contact</li>
+            </ul>
+        </>
+    );
+};
+
+export default Navbar;
